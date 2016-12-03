84f1b0b74e1c5196cfbcb30489213c86c376e526
skunkworksllc <aaroncox@proactivequality.com>
2016-12-03 11:19:13 -0600

    Initial Commit

---
+++ WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttongenerate = new System.Windows.Forms.Button();
+            this.textbox = new System.Windows.Forms.TextBox();
+            this.lblStatus = new System.Windows.Forms.Label();
+            this.buttonExit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // buttongenerate
+            // 
+            this.buttongenerate.Location = new System.Drawing.Point(13, 39);
+            this.buttongenerate.Name = "buttongenerate";
+            this.buttongenerate.Size = new System.Drawing.Size(166, 23);
+            this.buttongenerate.TabIndex = 0;
+            this.buttongenerate.Text = "Generate";
+            this.buttongenerate.UseVisualStyleBackColor = true;
+            this.buttongenerate.Click += new System.EventHandler(this.buttongenerate_Click);
+            // 
+            // textbox
+            // 
+            this.textbox.Location = new System.Drawing.Point(13, 13);
+            this.textbox.Name = "textbox";
+            this.textbox.Size = new System.Drawing.Size(166, 20);
+            this.textbox.TabIndex = 1;
+            this.textbox.Text = "Enter Text Here";
+            this.textbox.Enter += new System.EventHandler(this.textbox_Enter);
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.AutoSize = true;
+            this.lblStatus.Location = new System.Drawing.Point(80, 78);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(24, 13);
+            this.lblStatus.TabIndex = 2;
+            this.lblStatus.Text = "Idle";
+            // 
+            // buttonExit
+            // 
+            this.buttonExit.Location = new System.Drawing.Point(55, 117);
+            this.buttonExit.Name = "buttonExit";
+            this.buttonExit.Size = new System.Drawing.Size(75, 23);
+            this.buttonExit.TabIndex = 3;
+            this.buttonExit.Text = "Close";
+            this.buttonExit.UseVisualStyleBackColor = true;
+            this.buttonExit.Click += new System.EventHandler(this.buttonExit_Click);
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(191, 152);
+            this.ControlBox = false;
+            this.Controls.Add(this.buttonExit);
+            this.Controls.Add(this.lblStatus);
+            this.Controls.Add(this.textbox);
+            this.Controls.Add(this.buttongenerate);
+            this.Name = "frmMain";
+            this.Text = "Demo App";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttongenerate;
+        private System.Windows.Forms.TextBox textbox;
+        private System.Windows.Forms.Label lblStatus;
+        private System.Windows.Forms.Button buttonExit;
+    }
+}
+
---
+++ WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.Office.Interop;
+using System.IO;
+
+namespace WindowsFormsApplication1
+{
+    public partial class frmMain : Form
+    {
+
+        private object path = "C:\\data\\temp.docx";
+        private object saveAs = "C:\\data\\test.docx";
+
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void buttongenerate_Click(object sender, EventArgs e)
+        {
+            string jobName = textbox.Text.Trim();//trimming spaces off front and rear
+            if (InputGood(jobName))
+            {
+                lblStatus.Text = "Generating";
+                if (Generate(jobName, path))
+                    lblStatus.Text = "Generateion Complete";
+                else
+                    lblStatus.Text = "Generation Failed";                
+            }
+            else
+            {
+                lblStatus.Text = "Check input.";
+                return; // bail out
+            }
+
+        }
+
+        private void buttonExit_Click(object sender, EventArgs e)
+        {
+            this.Dispose();
+            this.Close();
+        }
+
+      
+        private bool InputGood(string s)
+        {
+            return s.Trim().Length > 0;    //we are returning whether the number of characters is greater than 0
+        }
+
+
+        public bool Generate(string text, object path)
+        {
+            bool success = false;// default to failed
+            object missing = System.Reflection.Missing.Value;
+            Microsoft.Office.Interop.Word.Application wordApp = new Microsoft.Office.Interop.Word.Application();
+            Microsoft.Office.Interop.Word.Document aDoc = null;
+            if(File.Exists((string)path))
+            {
+                object readOnly = false;
+                object isVisible = false;
+                wordApp.Visible = false;
+                aDoc = wordApp.Documents.Open(ref path, ref missing, ref readOnly, ref missing, ref missing, ref missing, ref missing, ref missing, ref missing, ref missing, ref missing, ref isVisible, ref missing, ref missing, ref missing, ref missing);
+                aDoc.Activate();
+
+                //replace text
+                ReplaceText(wordApp, "<jobname>", text);
+
+
+            }
+            else
+            {
+                MessageBox.Show("File Does Not Exist");
+                return success; 
+            }
+
+            aDoc.SaveAs(ref saveAs, ref missing, ref missing, ref missing, ref missing, ref missing, ref missing, ref missing, ref missing, ref missing, ref missing, ref missing, ref missing, ref missing, ref missing);
+            success = true;
+
+            aDoc.Close(ref missing, ref missing, ref missing);    
+
+
+            return success;
+        }
+
+
+        public static void ReplaceText(Microsoft.Office.Interop.Word.Application app, Object findMe, Object replaceMe)
+        {
+
+            try
+            {
+                object replaceAll = true;
+                object missing = System.Reflection.Missing.Value;
+                app.Application.Selection.Find.ClearFormatting();
+                app.Application.Selection.Find.Text = (string)findMe;
+                app.Application.Selection.Find.Replacement.ClearFormatting();
+
+
+                if (replaceMe.ToString().Length < 250) // Normal execution
+                {
+                    app.Application.Selection.Find.Replacement.Text = (string)replaceMe;
+                    app.Application.Selection.Find.Execute(
+                    ref missing, ref missing, ref missing, ref missing, ref missing,
+                    ref missing, ref missing, ref missing, ref missing, ref missing,
+                    ref replaceAll, ref missing, ref missing, ref missing, ref missing);
+                }
+
+                else  // Some real simple logic!!
+                {
+                    app.Application.Selection.Find.Execute(
+                    ref findMe, ref missing, ref missing, ref missing, ref missing,
+                    ref missing, ref missing, ref missing, ref missing, ref missing,
+                    ref missing, ref missing, ref missing, ref missing, ref missing);
+
+                    // the area where the findMe is located in the Document is selected.
+                    // Hence when you execute the statement below the replaceMe string is
+                    // placed in the area selected, which is what we wanted!
+                    app.Application.Selection.Text = (string)replaceMe;
+                }
+            }
+
+            catch (Exception ex)
+            {
+
+            }
+        }
+
+        private void textbox_Enter(object sender, EventArgs e)
+        {
+            textbox.Text = string.Empty;
+        }
+
+    }
+
+}
